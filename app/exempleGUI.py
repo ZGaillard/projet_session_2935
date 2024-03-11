ce3927270596a271e5934ca0275ee29e5c35f286
Zakary Gaillard-D <zakary.gaillard@outlook.com>
2024-03-11 10:52:03 -0400

    init repo

---
+++ app/exempleGUI.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Exemple de GUI")
+
+window.geometry("800x600")
+
+
+label = tk.Label(window, text="Hello World!")
+label.config(font=("Arial", 40))
+label.config(fg="blue")
+
+label.pack()
+
+window.mainloop()
