0587fdb118c2eff7dcb50522f3430da08a4d01e4
SamA1430 <samuel.arg14@gmail.com>
2024-04-05 18:31:34 -0400

    Refactor stored procedures to remove unnecessary ID parameters

--- database/DefAddArtist.sql
+++ database/DefAddArtist.sql
@@ -1,6 +1,6 @@
-CREATE OR ALTER PROCEDURE AddArtist @id INT, @nom VARCHAR(50), @prenom VARCHAR(50), @date_naissance DATE, @salaire_min INT, @domaine VARCHAR(50), @id_adresse INT
+CREATE OR ALTER PROCEDURE AddArtist @nom VARCHAR(50), @prenom VARCHAR(50), @date_naissance DATE, @salaire_min INT, @domaine VARCHAR(50), @id_adresse INT
 AS
 BEGIN
 INSERT INTO Artiste
-VALUES (@id,@nom,@prenom,@date_naissance,@salaire_min,@domaine,@id_adresse);
+VALUES (@nom,@prenom,@date_naissance,@salaire_min,@domaine,@id_adresse);
 END;
--- database/DefAddPlays.sql
+++ database/DefAddPlays.sql
@@ -1,8 +1,10 @@
-CREATE OR ALTER PROCEDURE AddPlays @id INT, @titre VARCHAR(50), @budget INT, @date_sortie DATE, @duree INT, @origine VARCHAR(50), @langue VARCHAR(50), @genre VARCHAR(50), @theatre VARCHAR(50)
+CREATE OR ALTER PROCEDURE AddPlays @titre VARCHAR(50), @budget INT, @date_sortie DATE, @duree INT, @origine VARCHAR(50), @langue VARCHAR(50), @genre VARCHAR(50), @theatre VARCHAR(50)
 AS
 BEGIN
 INSERT INTO Oeuvre
-VALUES (@id,@titre,@budget,@date_sortie,@duree,@origine,@langue,@genre);
+VALUES (@titre,@budget,@date_sortie,@duree,@origine,@langue,@genre);
+DECLARE @id INT;
+SET @id = SCOPE_IDENTITY();
 INSERT INTO Piece
 VALUES(@id, @theatre);
 END;
